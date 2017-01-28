087bc700e6cb9b0cac70c8314fcb3902cf02c1b7
bagf <rory@eishgaming.co.za>
2017-01-28 21:34:33 +0200

    Added pid config

---
+++ config/pidstatus.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /**
+     * Defaults work for Ubuntu 16.04 feel free to change this file
+     */
+    'nginx' => [
+        'pid' => env('NGINX_PID', '/var/run/nginx.pid'),
+        'name' => 'Nginx',
+    ],
+    'cron' => [
+        'pid' => env('CRON_PID', '/var/run/crond.pid'),
+        'name' => 'Cron',
+    ],
+    'ssh' => [
+        'pid' => env('SSH_PID', '/var/run/sshd.pid'),
+        'name' => 'SSH',
+    ],
+    'mysql' => [
+        'pid' => env('MYSQL_PID', '/var/run/mysqld/mysqld.pid'),
+        'name' => 'MYSQL',
+    ],
+];
