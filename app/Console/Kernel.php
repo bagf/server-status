c161e2e01d187b08d4961efd08c4ce7990fa457d
bagf <rory@eishgaming.co.za>
2017-01-28 22:36:49 +0200

    Scheduled update:stats command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Inspire::class,
         Commands\UpdateStats::class,
     ];
 
@@ -25,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->command('update:stats')
+            ->everyTenMinutes();
     }
 }
